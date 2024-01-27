c3fb6023433c1f3770f8880c5f3e3cf227956f04
“fathia <toohanour@gmail.com>
2024-01-27 15:00:32 +0300

    commmit msg

---
+++ admin.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+class admin extends user{
+    // public $uploadProduct="";
+
+    public function__construct($uploadProduct){
+        $this->$admin = $
+    }
+}
+?>
+</body>
+</html>
